e0d38483b159a8a9ce84742a4e1af11546692ba0
Kim Crawford <Kim@MacBook-Pro.local>
2019-10-15 16:32:16 +1300

    Lots of features

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -30,7 +31,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project',default=None, help="Only snapshots for project (tag: Project: <name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for volumes, not just the most recent")
+def list_snapshots(project, list_all):
     "List EBS snapshots volumes"
 
     instances = filter_instances(project)
@@ -42,6 +44,10 @@ def list_snapshots(project):
                 s.progress,
                 s.start_time.strftime("%c")
                 )))
+
+                if s.state == "completed" and not list_all: break
+
+
     return
 
 
@@ -111,7 +117,12 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}. ".format(i.id)+ str(e))
+            continue
+
     return
 
 @instances.command('start')
@@ -122,7 +133,12 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}. ".format(i.id)+ str(e))
+            continue
+
     return
 
 if __name__ == "__main__":
